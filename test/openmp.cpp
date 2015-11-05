25117b2e3ab24e1d1af6c354b0053a00b9bdeaaa
Hiroshi Maeda <inuhiroshi@gmail.com>
2015-11-05 13:29:07 +0900

    Add test of openMP

---
+++ test/openmp.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <omp.h>
+using namespace std;
+int main () {
+    int a = 0;
+#pragma omp parallel 
+    {
+        for (int i = 0; i < 2; i++) {
+#pragma omp single
+            {
+            a++;
+            cout << a << endl;
+            }
+#pragma omp for
+            for (int j = 0; j < 2; j++) {
+                cout << "a" << endl;
+                //cout << "2 " << omp_get_num_threads() << endl;
+            }
+        }
+    }
+}   
